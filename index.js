6a8f38afbad1707eb8251fc16daefa72d54a7c88
Antonio Scognamiglio <a.scognamiglio32@studenti.unisa.it>
2021-09-23 23:56:55 +0200

    feat: update repo url

--- index.js
+++ index.js
@@ -1,47 +1,51 @@
 #! /usr/bin/env node
-
 const { spawn } = require('child_process');
 
 const name = process.argv[2];
-if (!name || name.match(/[<>:"\/\\|?*\x00-\x1F]/)) {
-  return console.log(`
-  Invalid directory name.
-  Usage: create-express-api name-of-api  
-`);
-}
 
-const repoURL = 'https://github.com/w3cj/express-api-starter.git';
+const VALID_DIR_NAME = /[<>:"\/\\|?*\x00-\x1F]/;
+
+if (!name || name.match(VALID_DIR_NAME))
+	return console.log(`
+  Invalid directory name.
+  Usage: create-express-api-typescript <name-of-api>  
+`);
+
+const repoURL =
+	'https://github.com/xrenegade100/express-api-starter-typescript';
 
 runCommand('git', ['clone', repoURL, name])
-  .then(() => {
-    return runCommand('rm', ['-rf', `${name}/.git`]);
-  }).then(() => {
-    console.log('Installing dependencies...');
-    return runCommand('npm', ['install'], {
-      cwd: process.cwd() + '/' + name
-    });
-  }).then(() => {
-    console.log('Done! 🏁');
-    console.log('');
-    console.log('To get started:');
-    console.log('cd', name);
-    console.log('npm run dev');
-  });
+	.then(() => {
+		return runCommand('rm', ['-rf', `${name}/.git`]);
+	})
+	.then(() => {
+		console.log('Installing dependencies...');
+		return runCommand('npm', ['install'], {
+			cwd: process.cwd() + '/' + name
+		});
+	})
+	.then(() => {
+		console.log('Done! 🏁');
+		console.log('');
+		console.log('To get started:');
+		console.log('cd', name);
+		console.log('npm run dev');
+	});
 
 function runCommand(command, args, options = undefined) {
-  const spawned = spawn(command, args, options);
+	const spawned = spawn(command, args, { ...options, shell: true });
 
-  return new Promise((resolve) => {
-    spawned.stdout.on('data', (data) => {
-      console.log(data.toString());
-    });
-    
-    spawned.stderr.on('data', (data) => {
-      console.error(data.toString());
-    });
-    
-    spawned.on('close', () => {
-      resolve();
-    });
-  });
+	return new Promise((resolve) => {
+		spawned.stdout.on('data', (data) => {
+			console.log(data.toString());
+		});
+
+		spawned.stderr.on('data', (data) => {
+			console.error(data.toString());
+		});
+
+		spawned.on('close', () => {
+			resolve();
+		});
+	});
 }
